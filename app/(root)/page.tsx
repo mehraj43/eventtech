17b617b2580de45fa02eb994fc540d9ea96cb7b4
mohdmerajmalik <meraj@purecodesoftware.com>
2024-04-06 22:03:41 +0530

    hero img updated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -22,7 +22,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
   return (
     <>
       <section className='bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10'>
-        <div className='wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0'>
+        <div className='wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-3'>
           <div className='flex flex-col justify-center gap-8'>
             <h1 className='h1-bold'>
               Host, Connect, Celebrate: Your Events, Our Platform!
@@ -37,7 +37,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
           </div>
 
           <Image
-            src='/assets/images/hero.png'
+            src='/assets/images/event_tech.png'
             alt='hero'
             width={1000}
             height={1000}
